b00817e1cc484c458dc4ea1cd922ea5a6c5f2f09
Tirth-v <128678902+Tirth-v@users.noreply.github.com>
2023-09-28 15:48:14 +0530

    Add files via upload

---
+++ dataTypes.py
@@ -0,0 +1,95 @@
+# Data types
+
+"""
+    * type() function : To check data type of any variable.
+    
+    Datatype : In python data types are considered as 
+               classes and the variables we create those
+               are known as an instance or an object of that class.
+
+               eg. var1 = 3
+                   print(type(var1))
+                   o/p : <class 'int'>
+                            (Here var1 is an object of the class 'int')
+
+
+            - Data types represents the type of data stored
+              into a variable or memory.
+
+"""
+
+# Two types of datatypes :
+
+"""
+    1. Built-in datatype
+    2. User defined datatype
+
+
+
+    1. Built-in datatypes :
+        - These datatypes are already defined by python.
+        => Built-in datatypes :
+            - None type
+            - Numeric type
+                - int
+                - float
+                - complex number
+            - Dictionary
+            - Boolean
+            - Set
+            - Sequence datatype
+                - String
+                - List
+                - Tuple
+
+"""
+
+# None type :
+
+"""
+    - None datatype represents an object that does'nt contain any value.\
+    eg. a = None
+        print(type(a))
+        o/p : <class 'NoneType'>
+
+"""
+
+# int type :
+
+"""
+    - Integer datatype represents whole nunmbers(positive/negative)
+    ...-3,-2,-1,0,1,2,3...
+
+"""
+
+# float type :
+"""
+    - Float type represents floating point numbers(positive/negative)
+    ...-1.0,0.1,1.0,...
+
+"""
+
+# complex type :
+
+"""
+    - Complex number are written with "j" as the imaginary parth
+    - Used in many applications like
+        - Signal processing
+        - Algebra
+        - Physics
+
+    - eg. x = 1+2j
+
+"""
+
+# Boolean type :
+
+"""
+    - Data type with one of the two built-in values,
+      True or False.
+    - True and False with capital 'T' and 'F' are valid
+      booleans otherwise python will throw an error.
+
+"""
+
+# Remaining datatypes are included seperately to other files.
