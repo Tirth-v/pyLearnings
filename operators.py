9772f07f6623fd07777dc4d38b734c30a214b02e
Tirth-v <128678902+Tirth-v@users.noreply.github.com>
2023-09-28 19:15:29 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,106 @@
+# Operators are used to perform some operations between variables.
+
+"""
+    => Arithmetic operators
+    => Assignment operators
+    => Commparision operators
+    => Logical operators
+    => Identity operators
+    => Membership operators
+    => Bitwise operators
+"""
+
+# Arithmetic operators :
+"Precedence :"
+# PEMDAS
+# parantheses > 
+# (right->left)exponent  >
+# (left->right) multiplication, division, floor division, modulus >
+# (left->right) addition = subtraction
+
+"""
+        + addition
+        - subtraction
+        * multiplication
+        / division
+        % modulus(remainder)
+        ** exponent(power)
+        // floor division()
+
+"""
+
+# Assignment/comparision operator :
+
+"""
+        = store a value
+        == equal to
+        a=a+2 => a+=2
+        a=a/2 => a/=2
+        a=a-2 => a-=2
+        a=a*2 => a*=2
+        %=
+        //=
+        **=
+        &=
+        |=
+        >>=
+        <<=
+        <
+        >
+        >>
+        <<
+"""
+
+# Logical operators :
+
+"""
+    1. and
+    2. or
+    3. not
+
+    eg.
+        a = True
+        b = False
+        print(a and b)
+        >>False
+        print(a or b)
+        >>True
+        print(not a)
+        >>False
+        print(not b)
+        >>True
+
+"""
+
+# Identity operators :
+
+"""
+        1. is
+        2. is not
+
+        a = 5
+        b = 5
+        print(a is b)
+        >>True
+"""
+
+# Bitwise operator :
+
+"""
+        1. & and
+        2. | or
+        3. ^ xor
+        4. ~ not
+        5. << leftshift
+        6. >> rightshift
+
+
+"""
+
+# membership operator :
+
+"""
+        1. in
+        2. not in
+
+"""
