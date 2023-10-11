378e2e781de47d50837445356197536ee72e5b44
Tirth-v <128678902+Tirth-v@users.noreply.github.com>
2023-10-11 12:27:40 +0530

    Add files via upload

---
+++ Tirth_Datastructure_Test_1.py
@@ -0,0 +1,44 @@
+# Question 1 : Write a Python program to get the week number.
+
+import datetime
+
+
+def week_number():
+    try:
+        day = int(input("Enter the day :"))
+        month = int(input("Enter the month :"))
+        year = int(input("Enter the year :"))
+        week = datetime.date(year,month,day).isocalendar()[1]
+        print("The week of the date that you have entered is :")
+        return week
+    except ValueError:
+        print("Enter valid date")
+
+
+print(week_number())
+
+
+# Question 2 : Write a Python program to split a list every Nth element.
+
+
+def split_list():
+
+    for i in range(n):
+        lst1.append(lst[i::n])
+    print(lst1)
+
+while True:
+    try:
+        l = int(input("Enter the number of elements :"))
+        break
+    except ValueError:
+        print("Enter valid number of elements")
+lst = []
+for i in range(0,l):
+    ele = input()
+    lst.append(ele)
+print("The list that you have entered is :",lst)
+n = int(input("Enter the value of n :"))
+lst1 = []
+
+split_list()
